c63acb95b03cfc4b1d2f37a83fec356caa3b3477
Gael Hernandez <67879397+elgaeloHub@users.noreply.github.com>
2026-09-21 22:26:14 +0100

    Update curl_all_ix.bash

--- curl_all_ix.bash
+++ curl_all_ix.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-APIKEY=CAzdtz14.CTWkU79tBh6PcZzYjcyEWAntrrq1u00x
+APIKEY=<REPLACE_WITH_KEY>
 
 curl -H "Authorization: api-key $APIKEY" -sG https://www.peeringdb.com/api/ix | jq -r '.data[] | [.id, .name, .name_long, .city, .country, .region_continent, .net_count, .fac_count] | @csv' > all_ix.csv
 
